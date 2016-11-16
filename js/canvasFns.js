09f88ef476c1b39557cef3b07db16d288de37eb6
Cao Yuhao <117478646@qq.com>
2016-11-16 16:30:36 +0800

    aaa

--- js/canvasFns.js
+++ js/canvasFns.js
@@ -1,8 +1,12 @@
-function windowToCanvas(canvas, clientX, clientY) {
-    var boundingRect = canvas.getBoundingClientRect();
-    return {
-        //鼠标的x-boundingRect的x
-        x: (clientX - boundingRect.left) / (boundingRect.width / canvas.width), //boundingRect.width/canvas.width就是放大的倍数
-        y: (clientY - boundingRect.top) / (boundingRect.height / canvas.height)
+(function () {
+    var canvasFns = {};
+    canvasFns.windowToCanvas = function (canvas, clientX, clientY) {
+        var boundingRect = canvas.getBoundingClientRect();
+        return {
+            //鼠标的x-boundingRect的x
+            x: (clientX - boundingRect.left) / (boundingRect.width / canvas.width), //boundingRect.width/canvas.width就是放大的倍数
+            y: (clientY - boundingRect.top) / (boundingRect.height / canvas.height)
+        }
     }
-}
+    window.canvasFns = canvasFns;
+})()
